d63dff089d179b1da465bf5644e47dea5b749f2a
Aimless777 <anishgowda06@Gmail.com>
2023-05-22 09:51:43 -0700

    your favorite buttons favorite button

--- CrabClickerReal/src/Frame.java
+++ CrabClickerReal/src/Frame.java
@@ -1,3 +1,5 @@
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,7 +10,8 @@ import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
 public class Frame extends JFrame implements ActionListener {
-
+	JFrame frme = new JFrame("lol");
+	Container contentPane = frme.getContentPane();
 	private static long crabs;
 	private static long crabsPerSecond;
 	public static void main(String[] args) {
@@ -21,14 +24,23 @@ public class Frame extends JFrame implements ActionListener {
 	
 	public Frame() {
 		super("Cookie Clicker");
-
+		int l = 100;
+		int w = 100;
         crabs = 0;
 
+        int lx = 300;
+        int ly = 50;
+        int sx = 140;
+        int sy = 50;
+        
+        
         JLabel scoreLabel = new JLabel("Score: " + crabs);
         JButton cookieButton = new JButton("Click Me!");
-        cookieButton.setBounds(100, 100, 400, 400);
+        cookieButton.setPreferredSize(new Dimension(w, l));
+        cookieButton.setBounds(100, 100, 50, 50);
         JButton cursorButton = new JButton("Buy Cursor!");
-
+        cookieButton.setBounds(lx, ly, sx, sy);
+        frme.add(cookieButton);
         cookieButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 crabs++;
